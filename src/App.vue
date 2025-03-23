939d2da8d855f19a69c1109ef1af436a9a2eba5d
Mushegh Avetisyan <mushegh.avetisyan.web@gmail.com>
2025-03-24 00:35:16 +0400

    add analitics

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
+import { Analytics } from "@vercel/analytics/vue"
 </script>
 
 <template>
   <div>
     <Header />
     <router-view /> <!-- This will render the content based on the current route -->
+    <Analytics/>
     <Footer />
   </div>
 </template>
